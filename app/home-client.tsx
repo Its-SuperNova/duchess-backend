3599365b97af21b29a9b9a7000dff227f768517f
ashiwn <its.ashwin.23@gmail.com>
2025-07-21 17:14:57 +0530

    auth url changed from localhost to duchesspastryoes

--- app/home-client.tsx
+++ app/home-client.tsx
@@ -1,11 +1,30 @@
-import React from 'react'
+"use client";
 
-const HomeClient = () => {
-  return (
-    <div>
-      <h1>Home Client</h1>
-    </div>
-  )
+import { ProcessedProduct } from "@/lib/utils";
+
+import Hero from "@/components/block/hero";
+interface HomeClientProps {
+  initialProducts: ProcessedProduct[];
 }
 
-export default HomeClient
+interface ApiResponse {
+  success: boolean;
+  products: any[];
+}
+
+
+
+export default function HomeClient({ initialProducts }: HomeClientProps) {
+
+
+  return (
+    <div
+      className={`w-full overflow-x-hidden bg-white dark:bg-gray-900 `}
+    >
+      {/* Hero Section */}
+      <div className="w-full">
+        <Hero />
+      </div>
+    </div>
+  );
+}
